d0a491ce845e47215872cf67aa888dd23f0cbbbe
Diego Bejar <diebejardelaguila@gmail.com>
2022-12-10 09:46:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+from admin.router import admin_routs 
+from candidato.router import candidato_routs
+from institucion.router import institucion_routs
+from votante.router import votante_routs
+from voto.router import voto_routs
+from estadisticas.router import estadisticas_routs
+from app.router import app_routs
+
+
+app = FastAPI(
+    title="Api sistema de votacion",
+    contact={
+        "name": "Diego Bejar",
+        "email": "diebejardelaguila@gmail.com"
+    })
 
 class Msg(BaseModel):
     msg: str
@@ -25,3 +41,27 @@ async def demo_post(inp: Msg):
 @app.get("/path/{path_id}")
 async def demo_get_path_id(path_id: int):
     return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+
+
+origins = [
+    # your frontend port
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://localhost:4200",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(app_routs)
+app.include_router(admin_routs)
+app.include_router(candidato_routs)
+app.include_router(institucion_routs)
+app.include_router(votante_routs)
+app.include_router(voto_routs)
+app.include_router(estadisticas_routs)
