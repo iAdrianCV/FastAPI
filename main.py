f8845c2d1b409f839f297f9fb861e92ee8e616bf
Diego Bejar <diebejardelaguila@gmail.com>
2022-12-10 09:55:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,22 +27,6 @@ class Msg(BaseModel):
 async def root():
     return {"message": "Hello World. Welcome to FastAPI!"}
 
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
-
-
 origins = [
     # your frontend port
     "http://localhost:8000",
